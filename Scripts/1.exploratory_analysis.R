cf9fc9e5101ef5b4ad72a77e06fd4761cdbfce0b
Walter Chanava <walter.chanava@gmail.com>
2020-07-23 16:27:08 -0500

    Remove useless files

---
+++ Scripts/1.exploratory_analysis.R
@@ -0,0 +1,55 @@
+## Countries Analysis
+## Walter Chanava
+## Jul 2020
+
+
+# Packages ----------------------------------------------------------------
+
+library(dplyr)
+library(ggplot2)
+
+# Data --------------------------------------------------------------------
+
+covid <- read.csv("./Data/VariosPaises_Covid19.csv", sep = ";")
+covid_tbl <- covid %>% 
+  as.tbl() %>% 
+  filter(COUNTRY %in% c("PERU", "PANAMA", "MEXICO", "GUATEMALA", 
+                        "ELSALVADOR")) %>% 
+  mutate(country = as.factor(COUNTRY),
+         date = as.Date(FECHA, "%d/%m/%y"),
+         cases = as.numeric(CONTAGIADOS),
+         deaths = FALLECIDOS,
+         recov = as.numeric(RECUPERADOS),
+         hosps = as.numeric(HOSPITALIZADOS),
+         ucis = as.numeric(UCIs),
+         danger_idx = cases + deaths - recov) %>% 
+  select(country, date, cases, deaths, recov, hosps, ucis, danger_idx)
+
+
+# Plots -------------------------------------------------------------------
+
+source("./Scripts/custom_ggplot2_theme.R") # comment this line to avoid problems
+
+# Facet plot by country
+deaths_plot <- ggplot(data = covid_tbl, 
+                     aes(x = date, y = deaths, color = country)) +
+  geom_point() +
+  facet_wrap(~country, nrow = 3) +
+  theme(legend.position = 'none') +
+  custom_theme # comment this line to avoid 'fonts' problems
+
+ggsave(filename = "./Outputs/1.Deaths_by_day.tiff", plot = deaths_plot,
+       width = 6, height = 6)
+
+# Danger index by country
+danger_idx_plot <- ggplot(data = covid_tbl, 
+                          aes(x = date, y = danger_idx, color = country)) +
+  geom_point() +
+  geom_smooth(se = FALSE) +
+  facet_wrap(~country, nrow = 3) +
+  theme(legend.position = "none") +
+  labs(x = "Date", y = "Danger index") +
+  custom_theme # comment this line to avoid 'fonts' problems
+
+ggsave(filename = "./Outputs/2.Danger-idx-plot.tiff", plot = danger_idx_plot,
+       width = 6, height = 6)
